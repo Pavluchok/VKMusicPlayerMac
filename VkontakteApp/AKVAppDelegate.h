3bb81bcbdd418d53dc4d9df3be461a9b5f9fa751
Pavluchok <pavluchokv@gmail.com>
2014-11-06 12:16:33 +0200

    Initial Commit

---
+++ VkontakteApp/AKVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AKVAppDelegate.h
+//  VkontakteApp
+//
+//  Created by vitaliy.pavlyuk on 11/6/14.
+//  Copyright (c) 2014 vitaliy.pavlyuk. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AKVAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ VkontakteApp/AKVAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AKVAppDelegate.m
+//  VkontakteApp
+//
+//  Created by vitaliy.pavlyuk on 11/6/14.
+//  Copyright (c) 2014 vitaliy.pavlyuk. All rights reserved.
+//
+
+#import "AKVAppDelegate.h"
+
+@implementation AKVAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
